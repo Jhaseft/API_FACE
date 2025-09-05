a713093cfcf69d24a8824dc951ac89e0f7954cf1
Jhaseft <jhasesaat@gmail.com>
2025-09-04 23:29:51 -0400

    cors

--- api_kyc.py
+++ api_kyc.py
@@ -11,9 +11,14 @@ app = FastAPI(title="KYC Processor API")
 # =========================
 # CORS
 # =========================
+origins = [
+    "https://transfers.elchangarrodelima.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
